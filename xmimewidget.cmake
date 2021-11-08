29421cd54a08e502cc91a4d13a65e0599a9688aa
horsicq <horsicq@gmail.com>
2021-11-08 18:48:02 +0100

    Fix: 2021-11-08

--- xmimewidget.cmake
+++ xmimewidget.cmake
@@ -1,1 +1,13 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+include(${CMAKE_CURRENT_LIST_DIR}/../XMIME/xmime.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../XShortcuts/xshortcuts.cmake)
+
+set(XMIMEWIDGET_SOURCES
+    ${XMIME_SOURCES}
+    ${XSHORTCUTS_SOURCES}
+    ${CMAKE_CURRENT_LIST_DIR}/dialogmime.ui
+    ${CMAKE_CURRENT_LIST_DIR}/xmimewidget.ui
+    ${CMAKE_CURRENT_LIST_DIR}/dialogmime.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xmimewidget.cpp
+)
